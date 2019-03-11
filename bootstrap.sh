97995688e47cdee0d7555138823e1af05e985972
wildProgrammer <ianknote@gmail.com>
2019-03-11 15:23:19 +0200

    added selinux

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,4 +6,8 @@ rm -r $keylocation
 ssh-keygen -t rsa -f $keylocation/id_rsa -N 'no passphrase'
 cat $keylocation/id_rsa.pub >> $keylocation/authorized_keys
 
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config_backup
+echo "PasswordAuthentication no" >> /etc/ssh/sshd_config
+
+setenforce 0
 
